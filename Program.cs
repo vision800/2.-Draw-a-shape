6b0262bfab4cc95ff75070241583378d817900a4
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 09:56:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Draw_a_shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine(" ----");
+
+
+            Console.Write("where the cursor is ");
+
+            //FREEZE THE CONSOLE
+            Console.ReadLine();
+        }
+    }
+}
